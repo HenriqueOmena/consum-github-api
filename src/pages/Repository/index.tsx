4114871844419c535f49c6c15b5bced1f694fd5c
HenriqueOmena <88lmao@gmail.com>
2020-12-14 07:14:37 +0000

    Done styles for show single repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/githublogo.svg';
+import { Header, Issues, RepositoryInfo } from './style';
 
 interface Repository {
   repository: string;
@@ -10,8 +13,61 @@ const Repository: React.FC = () => {
 
   return (
     <div>
-      Repository:
-      {params.repository}
+      <Header>
+        <img src={logoImg} alt="logo consume github api" />
+        <Link to="/dashboard">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/35804326?s=460&u=458d2ff5326896d4dc02d2ffdbd76cdca025a248&v=4"
+            alt="Henrique OMena"
+          />
+          <div>
+            <strong>Henrique Omena</strong>
+            <p>desc of rep</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>001</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>001</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>001</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asddsa">
+          <div>
+            <strong>asdasd</strong>
+            <p>xzcxczxczxc</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+      <Issues>
+        <Link to="asddsa">
+          <div>
+            <strong>asdasd</strong>
+            <p>xzcxczxczxc</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </div>
   );
 };
